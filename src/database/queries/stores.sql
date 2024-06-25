ceb01b829e08f1913db89596c5de35f141167dc9
Margarida Grilo <margaridagrilo7@gmail.com>
2024-06-25 18:22:01 +0100

    database: added stores for initial database seeding (prisma)

--- src/database/queries/stores.sql
+++ src/database/queries/stores.sql
@@ -5,4 +5,12 @@ VALUES
   ('Bubble Time', 'Bubble Time was established in September 2022 from the merger of Bubble Lab and Time Tea and Coffee', 'Rua da Pimenta 3, Lisbon 1990-254 Portugal', 38.76993, -9.09246),
   ('Mommy Cake Bubbletea', '...', 'R. do Crucifixo 112 114, 1100-185 Lisboa', 38.71135, -9.13917),
   ('Bubble Tea Factory Lisbon', 'The Bubble Tea Factory creates bubbles, flavours, fun experiences, unforgettable moments for you that are visiting Portugal or/and who wants to drink something unique. The Bubble Tea Factory is a Portuguese brand, and its uniqueness is to promote the Portuguese flavours. We are mixing the traditional Taiwanese concept with Portuguese flavours to create an unique experience.', 'Tv. Marta Pinto 14, 1300-315 Lisboa', 38.69765, -9.20251),
-  ('CONBINI', 'A Conbini é uma loja especializada em Merchandise, Figuras e Model Kits. Conta com uma loja física em Lisboa desde Outubro de 2022 e com uma loja online desde 2024.', 'Avenida Ressano Garcia 39A, 1070-234 Lisboa', 38.73489, -9.15705);
+  ('CONBINI', 'A Conbini é uma loja especializada em Merchandise, Figuras e Model Kits. Conta com uma loja física em Lisboa desde Outubro de 2022 e com uma loja online desde 2024.', 'Avenida Ressano Garcia 39A, 1070-234 Lisboa', 38.73489, -9.15705),
+  ('Bubble Time Baixa Chiado','Bubble Time was established in September 2022 from the merger of Bubble Lab and Time Tea and Coffee.', 'Rua da Prata n160, 1100-416 Lisboa', 38.71142,-9.13701),
+  ('Mr. Box Tea','Mr.Box Tea Lisboa: Bubble Tea, Fruit Tea, Milk Tea e muitos Mochis!', 'Av. António Augusto de Aguiar 140B, 1050-021 Lisboa',38.73483, -9.15407 ),
+  ('Bubble Time Saldanha','Bubble Time was established in September 2022 from the merger of Bubble Lab and Time Tea and Coffee.', 'Av. Praia da Vitória 50A, 1050-184 Lisboa', 38.73366,-9.14628),
+  ('Lili Boba', 'Bubble tea shop with authentic bobas from Taiwan. Homemade syrup, fresh fruit based with matcha, coffee and choices of plant based milk. Handmade cupcakes and croffles on offer. With made to order floral artisan cupcakes, and Korean style croffles or croiffles with strawberry and cream.', 'R. de São Bento 68, 1200-815 Lisboa', 38.71099, -9.15256),
+  ('TwoToo Cais do Sodré', '...', 'PT, Cais do Sodré 12, 1249-289 Lisboa', 38.70669, -9.14237),
+  ('Bubble Time Roma', 'Bubble Time was established in September 2022 from the merger of Bubble Lab and Time Tea and Coffee.', 'Av. de Roma 22B, 1000-266 Lisboa', 38.74424, -9.13894),
+  ('Mon Tea Portugal 萌', '...', 'Av. Aquilino Ribeiro Machado F2, 1990-084 Lisboa', 
+  38.76844, -9.09914);
